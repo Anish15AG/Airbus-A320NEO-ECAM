59b8b46112bba2ae424408e99253def14d94f89f
Anish <anish.agclg@gmail.com>
2025-04-28 05:05:26 +0530

    feat: Simulation of data for Altitude, PSI, V/S components of flight

--- core/cabin_press.py
+++ core/cabin_press.py
@@ -6,4 +6,15 @@ class CabinPressureSystem:
         self.delta_pressure = 0 #Differential Pressure (PSI)
         self.vertical_speed = 0 #Vertical Speed ft/m
     
-    
+    def update(self):
+        """Simulating with data for real time updates on cabin pressurizartion"""
+        self.cabin_altitude = random.uniform(0,8000) #Simulation of altitude params upto 8000ft
+        self.delta_pressure = random.uniform(7,9) #Simulation of PSI @ 7/9
+        self.vertical_speed = random.uniform(-1000, 1100) #Verical speed limits of descent and climb perf
+
+    def get_data(self):
+        return{
+            "Cabin Altitude": self.cabin_altitude,
+            "Delta Pressure PSI": self.delta_pressure,
+            "Verical Speed":self.vertical_speed
+        }
