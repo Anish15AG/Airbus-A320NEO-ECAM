9da641ab89269febd68b078cb9af85e88bfb0e32
Anish <anish.agclg@gmail.com>
2025-04-28 05:01:21 +0530

    Initialized cabin_press with basic parameters

---
+++ core/cabin_press.py
@@ -0,0 +1,9 @@
+import random
+
+class CabinPressureSystem:
+    def __init__(self):
+        self.cabin_altitude = 0 #Cabin Altitude (feet)
+        self.delta_pressure = 0 #Differential Pressure (PSI)
+        self.vertical_speed = 0 #Vertical Speed ft/m
+    
+    
